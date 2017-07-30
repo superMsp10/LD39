ede617516f976ad48166ded751b140f63126bd6c
superMsp10 <pandeymahan34@gmail.com>
2017-07-30 19:51:51 -0400

    instruction

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,7 +9,6 @@ public class GameManager : MonoBehaviour
     public static GameManager thisM;
     public int levelNum = -1;
     public string[] levelScenes;
-    public string startScene, uiScene, endScene;
     public Level currLvl;
     public int loseComplaintNum = 5;
     public int totalComplaints, totalOrders, totalDeliveries, totalPower;
@@ -30,7 +29,8 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.F5)){
+        if (Input.GetKeyUp(KeyCode.F5))
+        {
             restartLevel();
         }
     }
@@ -44,30 +44,9 @@ public class GameManager : MonoBehaviour
 
     public void nextLevel()
     {
-        if (levelNum == levelScenes.Length)
-        {
-            endCutscene();
-        }
-        else
-        {
-            SceneManager.LoadScene(levelNum++);
-        }
+        SceneManager.LoadScene(levelScenes[1]);
     }
 
-    public void startCutscene()
-    {
-        SceneManager.LoadScene(startScene);
-    }
-
-    public void loadUiScene()
-    {
-        SceneManager.LoadScene(uiScene);
-    }
-
-    public void endCutscene()
-    {
-        SceneManager.LoadScene(endScene);
-    }
     public void changeLevel(Level newLvl)
     {
         currLvl = newLvl;
